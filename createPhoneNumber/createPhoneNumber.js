15eb87e0130559d5d326ac9b57ca89aa234adb14
rjaimes <rafael.jaimes@housecallpro.com>
2022-09-12 18:05:52 -0500

    Add Errors.

--- createPhoneNumber/createPhoneNumber.js
+++ createPhoneNumber/createPhoneNumber.js
@@ -1,11 +1,11 @@
 class PhoneNumber {
   digits;
   constructor(input = []) {
-    if (input.length != 10) throw 'Phone number must be 10 digits long';                        // must be 10 characters/numbers long.
+    if (input.length != 10) throw Error('Phone number must be 10 digits long');                        // must be 10 characters/numbers long.
     if (input.some(n => (typeof n != 'string' && typeof n != 'number')                          // must be a string or a number.
       || (typeof n != 'number' && !n)                                                           // must not be empty.
-      || (typeof n == 'number' && isNaN(n)))) throw 'only string and number types allowed';     // must not be NaN.
-    if (input.some(n => String(n).length != 1)) throw 'invalid format';                         // must be single characters/digits per index.
+      || (typeof n == 'number' && isNaN(n)))) throw 'Only string and number types allowed';     // must not be NaN.
+    if (input.some(n => String(n).length != 1)) throw 'Invalid format';                         // must be single characters/digits per index.
     this.digits = this.cleanup(input);
   }
 
--- createPhoneNumber/test.js
+++ createPhoneNumber/test.js
@@ -1,7 +1,7 @@
-const assert = require('assert');
+const { assert } = require('chai');
 const PhoneNumber = require('./createPhoneNumber');
 
-describe('PhoneNumer', function () {
+describe('PhoneNumber', function () {
   describe('#build()', function () {
     it('should build phone number from array of numbers', function () {
       assert.equal(new PhoneNumber([1,2,3,4,5,6,7,8,9,0]).build(), '(123) 456-7890');
@@ -9,9 +9,24 @@ describe('PhoneNumer', function () {
     });
     it('should build phone number from array of characters', function () {
       assert.equal(new PhoneNumber(['¡','l','l','a','m','e',' ','y','a','!']).build(), '(155) 263-0921');
+      assert.equal(new PhoneNumber(['3','1','2','l','l','a','m','e','y','a']).build(), '(312) 552-6392');
     });
     it('should build phone number from array of characters and numbers', function () {
       assert.equal(new PhoneNumber([3,1,2,'l','l','a','m','e','y','a']).build(), '(312) 552-6392');
+    });
+    it('should accept exactly 10 digits', function () {
+      assert.throws(() => new PhoneNumber([1,2,3,4,5,6,7]).build(),'Phone number must be 10 digits long');
+      assert.throws(() => new PhoneNumber([1,2,3,4,5,6,7,8,9,0,0,0,0,0]).build(),'Phone number must be 10 digits long');
+    });
+    it('should not accept more than 1 digit/character per index', function () {
+      assert.throws(() => new PhoneNumber([1,2,3,4,5,6,7,80,99,0]).build(),'Invalid format');
+    });
+    it('should accept only string and integer values', function () {
+      assert.throws(() => new PhoneNumber([1.4,2,3,4,5,6,7,8,9,0]).build(),'Invalid format');
+      assert.throws(() => new PhoneNumber([1,2,3,4,5,6,7,8,9,{foo:'bar'}]).build(),'Only string and number types allowed');
+      assert.throws(() => new PhoneNumber([1,2,3,4,5,6,7,8,9,NaN]).build(),'Only string and number types allowed');
+      assert.throws(() => new PhoneNumber([1,2,3,4,5,6,7,8,9,null]).build(),'Only string and number types allowed');
+      assert.throws(() => new PhoneNumber([1,2,3,4,5,6,7,8,9,undefined]).build(),'Only string and number types allowed');
     });
   });
 });
